3e727f8d70f342cd017747bb94e0e972a32a3326
marc.friedli <marc.friedli@he-arc.ch>
2017-04-05 14:42:22 +0200

    Ajout des fichiers pommes

---
+++ tp2_project/pomme.cpp
@@ -0,0 +1,13 @@
+#include "pomme.h"
+
+#include <iostream>
+
+Pomme::Pomme():Fruit("Pomme",true)
+{
+    //ctor
+}
+
+Pomme::~Pomme()
+{
+    //dtor
+}
---
+++ tp2_project/pomme.h
@@ -0,0 +1,16 @@
+#ifndef POMME_H
+#define POMME_H
+
+#include "fruit.h"
+
+
+class Pomme : public Fruit
+{
+    public:
+        Pomme();
+        ~Pomme();
+    protected:
+    private:
+};
+
+#endif // POMME_H
