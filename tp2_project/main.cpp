727a884a2303b259d03f831a03cc5816c9c656e5
marc.friedli <marc.friedli@he-arc.ch>
2017-04-18 15:14:54 +0200

    Ajout du patron décorateur

--- tp2_project/main.cpp
+++ tp2_project/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <list>
 #include "banane.h"
+#include "pomme.h"
+#include "fraise.h"
 #include "fruit.h"
 
 using namespace std;
 
 int main()
 {
-    Banane *banane = new Banane();
+    std::list<Fruit*> panier;
+    panier.push_back(new Banane());
+    panier.push_back(new Pomme());
+    panier.push_back(new Fraise());
+    panier.push_back(new Pomme());
+
+    list<Fruit*>::iterator it;	//ou auto it = lst.begin()
+    for (it = panier.begin(); it != panier.end(); it++)
+        cout << (*it)->toString() << endl;
+
     return 0;
 }
